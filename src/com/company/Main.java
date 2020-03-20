f2a49336e68c28309768a2881e6d9c9ce9edd9f5
Szili <nagyszili14@gmail.com>
2020-03-20 19:46:03 +0200

    No Random

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class Main {
 
+    private final static int[][] goal = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+    //    final static int[][] goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    private static int visitedNodeCounter = 0;
+    private static int costCounter = 0;
+    private static boolean readFile = false; // -input "filename.txt"
+    private static boolean writeToStdOutSeq = false; // -solseq
+    private static boolean writeToStdOutCost = false; // -pcost
+    private static boolean writeToStdOutVisited = false; // -nvisited
+    private static int heuristicType = 1; // -h 1 / -h 2
 
     public static void main(String[] args) {
 
         int[][] start = new int[3][3];
-        int[][] goal = {{0,1,2},{3,4,5},{6,7,8}};
-        List<Node> Open = new ArrayList<>();
-        List<Node> Closed = new ArrayList<>();
+//        int[][] goal = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+        List<Node> openList = new ArrayList<>();
+        List<Node> closedList = new ArrayList<>();
         String file = "";
-        boolean readFile = false; // -input "filename.txt"
-        boolean writeToStdOutSeq = false; // -solseq
-        boolean writeToStdOutCost = false; // -pcost
-        boolean writeToStdOutVisited = false; // -nvisited
-        int heuristic = 1; // -h 1 / -h 2
+
 
         if (args.length != 0) {
             for (int i = 0; i < args.length; ++i) {
@@ -28,7 +35,6 @@ public class Main {
                     case "-input": {
                         file = args[++i];
                         readFile = true;
-                        System.out.println(file);
                         break;
                     }
                     case "-solseq": {
@@ -46,7 +52,7 @@ public class Main {
                     }
 
                     case "-h": {
-                        heuristic = Integer.parseInt(args[++i]);
+                        heuristicType = Integer.parseInt(args[++i]);
                         break;
                     }
                 }
@@ -58,10 +64,10 @@ public class Main {
             try {
                 File myObj = new File(file);
                 Scanner reader = new Scanner(myObj);
-                while (reader.hasNextInt()) {
-//                    String data = reader.nextLine();
-                    int num = reader.nextInt();
-                    System.out.println(num);
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        start[i][j] = reader.nextInt();
+                    }
                 }
                 reader.close();
             } catch (FileNotFoundException e) {
@@ -69,29 +75,184 @@ public class Main {
                 e.printStackTrace();
             }
         } else {
-
-            Scanner in = new Scanner(System.in);
-
-            while (in.hasNextInt()) {
-                System.out.println(in.nextInt());
+            System.out.println("Pleas enter the puzzle!");
+            Scanner reader = new Scanner(System.in);
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    start[i][j] = reader.nextInt();
+                }
             }
         }
 
+        aStar(start, heuristicType, openList, closedList);
+
     }
 
 
-    public void aStar(int[][] startState) {
+    public static Node aStar(int[][] startState, int heuristicType, List<Node> openList, List<Node> closedList) {
+
         Node startNode = new Node(startState);
+        System.out.println("Start Node");
+        startNode.printNode();
+        System.out.println("The Goal I want to reach");
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                System.out.print(goal[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
         startNode.setG(0);
+        startNode.setH(getHeuristic(startState, heuristicType));
+        startNode.setF(startNode.getG() + startNode.getH());
+        startNode.setParent(null);
+        openList.add(startNode);
+
+        Node currentNode;
+
+        while (!openList.isEmpty()) {
+
+            currentNode = getLowestFNode(openList);
+            closedList.add(currentNode);
+            openList.remove(currentNode);
+
+            ++visitedNodeCounter;
+
+            if (currentNode.isGoal()) {
+                System.out.println("The Goal is Reached");
+                if (writeToStdOutVisited) {
+                    System.out.println("Visited Nodes: " + visitedNodeCounter);
+                }
+                if (writeToStdOutCost) {
+                    System.out.println("Cost: " + currentNode.getG());
+                }
+
+
+                return currentNode;
+            }
+
+            if (writeToStdOutSeq) {
+                currentNode.printNode();
+            }
+
+
+            for (int i = 0; i < 4; ++i) {
+                Node child = slide(currentNode, i);
+                if (child == null) {
+                    continue;
+                }
+                child.setH(getHeuristic(child.getState(), heuristicType));
+                child.setF(child.getG() + child.getH());
+
+                Node X = find(openList, child);
+                if (X != null) {
+                    if (child.getF() <= X.getF()) {
+                        continue;
+                    } else {
+                        openList.remove(X);
+                    }
+                }
+                Node Y = find(closedList, child);
+                if (Y != null) {
+                    if (child.getF() <= Y.getF()) {
+                        continue;
+                    } else {
+                        closedList.remove(Y);
+                    }
+                }
+                openList.add(child);
+
+            }
+
+        }
+
+        return null;
 
 
     }
 
-    public Node getLowestFNode(List<Node> Open) {
+    private static Node find(List<Node> list, Node child) {
+
+        for (Node node : list) {
+            if (child.compareState(node)) {
+                return child;
+            }
+        }
+
+        return null;
+    }
+
+    private static Node slide(Node parent, int toDirection) {
+
+        Node node = new Node();
+        int[][] state = new int[parent.getState().length][];
+        for (int i = 0; i < parent.getState().length; i++) {
+            state[i] = parent.getState()[i].clone();
+        }
+
+        int i = parent.getPositionOfZero()[0], j = parent.getPositionOfZero()[1];
+
+
+        switch (toDirection) {
+            case 0: {  //move up
+                if (i >= 1) {
+                    int up = state[i - 1][j];
+                    state[i][j] = up;
+                    state[i - 1][j] = 0;
+                } else {
+                    return null;
+                }
+
+                break;
+            }
+            case 1: {  //move down
+                if (i < 2) {
+                    int down = state[i + 1][j];
+                    state[i][j] = down;
+                    state[i + 1][j] = 0;
+                } else {
+                    return null;
+                }
+
+                break;
+            }
+            case 2: {  //move left
+                if (j >= 1) {
+                    int left = state[i][j - 1];
+                    state[i][j] = left;
+                    state[i][j - 1] = 0;
+                } else {
+                    return null;
+                }
+
+                break;
+            }
+            case 3: {  //move right
+                if (j < 2) {
+                    int right = state[i][j + 1];
+                    state[i][j] = right;
+                    state[i][j + 1] = 0;
+                } else {
+                    return null;
+                }
+
+                break;
+            }
+        }
+
+        node.setParent(parent);
+        node.setState(state);
+        node.setG(parent.getG() + 1);
+
+        return node;
+    }
+
+    public static Node getLowestFNode(List<Node> open) {
         Node minF = null;
-        if (!Open.isEmpty()) {
-            minF = Open.get(0);
-            for (Node node : Open) {
+        if (!open.isEmpty()) {
+            minF = open.get(0);
+            for (Node node : open) {
                 if (node.getF() < minF.getF()) {
                     minF = node;
                 } else if (node.getF() == minF.getF() && node.getH() < minF.getH()) {
@@ -102,19 +263,44 @@ public class Main {
         return minF;
     }
 
-    public int heuristicOne(int[][] goal, int[][] state) {
-
+    public static int getHeuristic(int[][] state, int heuristic) {
         int h = 0;
+        if (heuristic == 1) {
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (state[i][j] != goal[i][j] && state[i][j] != 0) {
-                    h++;
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (state[i][j] != goal[i][j] && state[i][j] != 0) {
+                        h++;
+                    }
+                }
+            }
+
+            return h;
+        } else if (heuristic == 2) {
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    if (state[i][j] != 0) {
+                        h += manhattanDistance(state, i, j);
+                    }
+                }
+            }
+            return h;
+        }
+
+        return 0;
+    }
+
+    public static int manhattanDistance(int[][] state, int x, int y) {
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (goal[i][j] == state[x][y]) {
+                    return abs(x - i) + abs(y - j);
                 }
             }
         }
-
-        return h;
+        return 0;
     }
 
 
--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,20 +1,23 @@
 package com.company;
 
 public class Node {
-    private int[][] puzzle;
+    private int[][] state;
     private int g;
     private int h;
     private int f;
     private Node parent;
-    private int[][] start;
-    private int[][] goal;
+    private int[][] goal = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+//    private int[][] goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 
-    public Node(int[][] puzzle) {
-        this.puzzle = puzzle;
+    public Node() {
     }
 
-    public Node(int[][] puzzle, Node parent) {
-        this.puzzle = puzzle;
+    public Node(int[][] state) {
+        this.state = state;
+    }
+
+    public Node(int[][] state, Node parent) {
+        this.state = state;
         this.parent = parent;
     }
 
@@ -41,4 +44,70 @@ public class Node {
     public void setF(int f) {
         this.f = f;
     }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public int[][] getState() {
+        return state;
+    }
+
+    public void setState(int[][] state) {
+        this.state = state;
+    }
+
+    public boolean isGoal() {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (this.state[i][j] != this.goal[i][j] && this.state[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int[] getPositionOfZero() {
+        int i = 0, j = 0;
+        int[] position = new int[2];
+        for (i = 0; i < 3; ++i) {
+            for (j = 0; j < 3; ++j) {
+                if (this.state[i][j] == 0) {
+                    position[0] = i;
+                    position[1] = j;
+                    return position;
+                }
+            }
+        }
+        return position;
+    }
+
+    public boolean compareState(Node node) {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (this.state[i][j] != node.getState()[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void printNode() {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                System.out.print(this.getState()[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
